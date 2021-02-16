10871d2d5ddd671c7764f6ad8a3b7d0088f28560
David Xu <david@Davids-MacBook-Air.local>
2021-02-15 22:58:13 -0800

    eslint

--- app.js
+++ app.js
@@ -12,6 +12,7 @@ const huddleRoutes = require('./routes/huddles')
 
 
 mongoose.connect(config.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+
   .then(() => {
     console.log('Connected')
   })
--- controllers/events.js
+++ controllers/events.js
@@ -3,30 +3,30 @@ let Huddle = require('./../models/huddle')
 var mongoose = require('mongoose')
 
 exports.createEvent = async function(req,res) {
-    //Req should give 
-    //event name/ address / interests /huddles
-    const newEvent = new Event(
-        {
-            eventCount : 1,
-            eventName : req.body.name,
-            eventAddress: req.body.address,
-            interests : ["Test"],
-            huddles : [mongoose.Types.ObjectId(req.body.huddleId)]
+  //Req should give 
+  //event name/ address / interests /huddles
+  const newEvent = new Event(
+    {
+      eventCount : 1,
+      eventName : req.body.name,
+      eventAddress: req.body.address,
+      interests : ['Test'],
+      huddles : [mongoose.Types.ObjectId(req.body.huddleId)]
 
 
 
     })
     
-    const savedEvent = await newEvent.save()
-    const huddle = await Huddle.findById(mongoose.Types.ObjectId(req.body.huddleId))
-    let eventForHuddle = JSON.parse(JSON.stringify(newEvent))
-    delete eventForHuddle.huddles
+  const savedEvent = await newEvent.save()
+  const huddle = await Huddle.findById(mongoose.Types.ObjectId(req.body.huddleId))
+  let eventForHuddle = JSON.parse(JSON.stringify(newEvent))
+  delete eventForHuddle.huddles
 
-    huddle.event = eventForHuddle
-    await huddle.save()
+  huddle.event = eventForHuddle
+  await huddle.save()
     
   
-    res.json(savedEvent)
+  res.json(savedEvent)
 
 }
 
